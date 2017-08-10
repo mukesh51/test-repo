01cbfaeedffea3d98dbc16572ec90352a5e5c10c
Mukesh G Chandnani <mukesh51@gmail.com>
2017-08-10 23:15:56 +0100

    index.php initial commit
    index.php initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>OpsWorks Test</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello PHP</h1>
+                <p>You're running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
